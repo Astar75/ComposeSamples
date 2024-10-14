af5d4d2a64b0c0b52ccbf3b2d85c0bd768ce2b12
Obit0 <shigin.rv@tk23.ru>
2024-10-14 16:41:24 +0300

    Так работает Column который занимает весь экран, если настроен модификатор с функцией fillMaxSize()

---
+++ app/src/main/java/ru/astar/composesamples/MainActivity.kt
@@ -0,0 +1,53 @@
+package ru.astar.composesamples
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import ru.astar.composesamples.ui.theme.ComposeSamplesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeSamplesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HelloWorld()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloWorld() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.background(color = Color.Yellow),
+            text = "Привет мир",
+            fontSize = 18.sp
+        )
+        Text(text = "Еще какой то текст")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HelloWorldPreview() {
+    ComposeSamplesTheme {
+        HelloWorld()
+    }
+}
